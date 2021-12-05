c0dd838a30f8d3f6171c38b8ee50e8c783cff7b3
Nacho <igvelasco@fi.uba.ar>
2021-12-05 12:27:33 -0300

    3 functions test on core

--- test/tp_final/core_test.clj
+++ test/tp_final/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [tp-final.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (deftest verificar-parentesis-test
+    (testing "FIXME, I fail."
+      (is (= 1 (verificar-parentesis "(hola 'mundo")))
+      (is (= -1 (verificar-parentesis "(hola '(mundo)))")))
+      (is (= -1 (verificar-parentesis "(hola '(mundo) () 6) 7)")))
+      (is (= -1 (verificar-parentesis "(hola '(mundo) () 6) 7) 9)")))
+      (is (= 0 (verificar-parentesis "(hola '(mundo) )")))
+    )
+  )
+          
+  (deftest buscar-test
+    (testing "FIXME, I fail."
+      (is (= 3 (buscar 'c '(a 1 b 2 c 3 d 4 e 5))))
+      (is (= (symbol "(;ERROR: unbound variable: f)") (buscar 'f '(a 1 b 2 c 3 d 4 e 5))))
+    )
+  )
+  
+  
+  (deftest error?-test
+    (testing "FIXME, I fail."
+      (is (= true (error? (list (symbol ";ERROR:") 'mal 'hecho))))
+      (is (= false (error? (list 'mal 'hecho))))
+      (is (= true (error? (list (symbol ";WARNING:") 'mal 'hecho))))
+    )
+  )
+            
